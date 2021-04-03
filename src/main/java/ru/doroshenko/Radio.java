79a950c2e980de9721da8fe43557f1120e968cac
Denis Doroshenko <ddtlt1984@gmail.com>
2021-04-03 19:18:00 +0400

    Added code & tests

--- src/main/java/ru/doroshenko/Radio.java
+++ src/main/java/ru/doroshenko/Radio.java
@@ -1,5 +1,104 @@
 package ru.doroshenko;
 
 public class Radio {
+    private int firstChannel = 0;
+    private int finalChannel = 9;
+    private int minVolume = 0;
+    private int maxVolume = 10;
+    private int currentChannel;
+    private int currentVolume;
 
+    public int getFirstChannel() {
+        return firstChannel;
+    }
+
+    public void setFirstChannel(int firstChannel) {
+        this.firstChannel = firstChannel;
+    }
+
+    public int getFinalChannel() {
+        return finalChannel;
+    }
+
+    public void setFinalChannel(int finalChannel) {
+        this.finalChannel = finalChannel;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public int getCurrentChannel() {
+        return currentChannel;
+    }
+
+    public void setCurrentChannel(int currentChannel) {
+        if (currentChannel > finalChannel) {
+            this.currentChannel = finalChannel;
+            return;
+        }
+        if (currentChannel < firstChannel) {
+            this.currentChannel = firstChannel;
+            return;
+        }
+        this.currentChannel = currentChannel;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > maxVolume){
+            this.currentVolume = maxVolume;
+            return;
+        }
+        if (currentVolume < minVolume){
+            this.currentVolume = minVolume;
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume == maxVolume) {
+            return;
+        }
+        currentVolume++;
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume == minVolume) {
+            return;
+        }
+        currentVolume--;
+    }
+
+    public void switchChannelUp() {
+        if (currentChannel == finalChannel) {
+            this.currentChannel = firstChannel;
+            return;
+        }
+        currentChannel++;
+    }
+
+    public void switchChannelDown() {
+        if (currentChannel == firstChannel) {
+            this.currentChannel = finalChannel;
+            return;
+        }
+        currentChannel--;
+    }
 }
--- src/test/java/ru/doroshenko/RadioTest.java
+++ src/test/java/ru/doroshenko/RadioTest.java
@@ -1,7 +1,100 @@
 package ru.doroshenko;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
+    Radio radio = new Radio();
 
+    @Test
+    void setCurrentChannel() {
+        radio.setCurrentChannel(5);
+        assertEquals(5, radio.getCurrentChannel());
+    }
+
+    @Test
+    void setCurrentChannelOverFinal() {
+        radio.setCurrentChannel(15);
+        assertEquals(9, radio.getCurrentChannel());
+    }
+
+    @Test
+    void setCurrentChannelLessFirst() {
+        radio.setCurrentChannel(-5);
+        assertEquals(0, radio.getCurrentChannel());
+    }
+
+    @Test
+    void setCurrentVolume() {
+        radio.setCurrentVolume(5);
+        assertEquals(5, radio.getCurrentVolume());
+    }
+
+    @Test
+    void setCurrentVolumeOverMax() {
+        radio.setCurrentVolume(15);
+        assertEquals(10, radio.getCurrentVolume());
+    }
+
+    @Test
+    void setCurrentVolumeLessMin() {
+        radio.setCurrentVolume(-5);
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    void increaseVolume() {
+        radio.setCurrentVolume(5);
+        radio.increaseVolume();
+        assertEquals(6, radio.getCurrentVolume());
+    }
+
+    @Test
+    void increaseVolumeOverMax() {
+        radio.setCurrentVolume(radio.getMaxVolume());
+        radio.increaseVolume();
+        assertEquals(10, radio.getCurrentVolume());
+    }
+
+    @Test
+    void decreaseVolume() {
+        radio.setCurrentVolume(5);
+        radio.decreaseVolume();
+        assertEquals(4, radio.getCurrentVolume());
+    }
+@Test
+    void decreaseVolumeLessMin() {
+        radio.setCurrentVolume(radio.getMinVolume());
+        radio.decreaseVolume();
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    void switchChannelUp() {
+        radio.setCurrentChannel(5);
+        radio.switchChannelUp();
+        assertEquals(6, radio.getCurrentChannel());
+    }
+
+    @Test
+    void switchChannelUpOverFinal() {
+        radio.setCurrentChannel(radio.getFinalChannel());
+        radio.switchChannelUp();
+        assertEquals(0, radio.getCurrentChannel());
+    }
+
+    @Test
+    void switchChannelDown() {
+        radio.setCurrentChannel(5);
+        radio.switchChannelDown();
+        assertEquals(4, radio.getCurrentChannel());
+    }
+
+    @Test
+    void switchChannelDownLessFirst() {
+        radio.setCurrentChannel(radio.getFirstChannel());
+        radio.switchChannelDown();
+        assertEquals(9, radio.getCurrentChannel());
+    }
 }
